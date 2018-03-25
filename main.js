748f78ac9117b78b6cc65f6f78fb4859233955cc
Charlotte Cullip <charlotte.cullip@gmail.com>
2018-03-25 16:29:09 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+var id = setInterval(frame, 5);
+
+function frame() {
+    if (/* test for finished */) {
+        clearInterval(id);
+    } else {
+        /* code to change the element style */
+    }
+}
+
+function myMove() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
