1f04a3f97717244cec5d8f8b5d486e71aa3e72ae
Charlotte Cullip <charlotte.cullip@gmail.com>
2018-04-08 19:30:32 -0700

    complete design

--- main.js
+++ main.js
@@ -2,10 +2,20 @@
 var goingRight = true;
 var running = false;
 var expStart = 0;
+var probability = 0;
+var tunneling1True = false;
+var tunneling2True = false;
+var tunneling3True = false;
+var restartValue = 0;
+var electronRight = true;
 
 function animationManager(flag, element, height, energy) {
     var elem = document.getElementById(element);
-
+    var tunneling1 = document.getElementById("tunneling1");
+    var tunneling2 = document.getElementById("tunneling2");
+    var tunneling3 = document.getElementById("tunneling3");
+    var barrier1 = document.getElementById("barrier1");
+    var barrier2 = document.getElementById("barrier2");
     if (flag == "start" && running == false) {
         running = true;
         var pos = elem.offsetLeft;
@@ -15,19 +25,11 @@ function animationManager(flag, element, height, energy) {
         function frame() {
             pos = elem.offsetLeft;
             if (goingRight == true) {
-                if (element == 'proton3') {
-                    // left wall traveling right
-                    if (pos < 100 && pos > 0) {
+                if (element == 'proton3' && (tunneling1.checked || tunneling1True)) {
+                    // right wall traveling right
+                    if (pos < 575 && pos > 475) {
+                        tunneling1True = true;
                         if (expStart > top) {
-                            elem.style.top = (top - expStart * Math.exp((pos - 100) / (-125 / Math.log(0.0001 * expStart)))) + 'px';
-                        } else {
-                            elem.style.top = (top - expStart * Math.exp((pos - 100) / (-125 / Math.log(0.0001 * expStart)))) + 'px';
-                        }
-                        pos++;
-                        elem.style.left = pos + 'px';
-                        // right wall traveling right
-                    } else if (pos < 575 && pos > 475) {
-                        if (expStart > top) {  
                             elem.style.top = (top + (expStart - top) * Math.exp(-(pos - 475) / (-125 / Math.log(0.0001 * (expStart - top))))) + 'px';
                         } else {
                             elem.style.top = (top - expStart * Math.exp(-(pos - 475) / (-125 / Math.log(0.0001 * expStart)))) + 'px';
@@ -36,7 +38,70 @@ function animationManager(flag, element, height, energy) {
                         pos++;
                         elem.style.left = pos + 'px';
                     } else if (pos == 575) {
-                        goingRight = false;
+                        flag = 'stop';
+                        tunneling1True = false;
+                        animationManager(flag, element, height, energy);
+                    } else {
+                        elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 9))) + 'px';
+                        pos++;
+                        elem.style.left = pos + 'px';
+                        expStart = (top + (parseInt(slider.value) * Math.sin(pos / 9)));
+                    }
+                } else if (element == 'proton4' && (tunneling2.checked || tunneling2True)) {
+                    // right wall traveling right
+                    if (pos < 480 + parseInt(barrier1.value) && pos > 475) {
+                        tunneling2True = true;
+                        if (expStart > top) {
+                            restartValue = (top + (expStart - top) * Math.exp(-(pos - 475) / (-900 / Math.log(0.0001 * (expStart - top)))));
+                            elem.style.top = restartValue + 'px';
+                            restartValue = restartValue % top;
+                        } else {
+                            restartValue = (top - expStart * Math.exp(-(pos - 475) / (-900 / Math.log(0.0001 * expStart))));
+                            elem.style.top = restartValue + 'px';
+                            restartValue = restartValue % top;
+                        }
+                        // travel exponentially
+                        pos++;
+                        elem.style.left = pos + 'px';
+                    } else if (pos == 875) {
+                        flag = 'stop';
+                        animationManager(flag, element, height, energy);
+                    } else if (pos >= 480 + parseInt(barrier1.value)) {
+                        elem.style.top = (top + (restartValue * Math.sin(pos / 9))) + 'px';
+                        pos++;
+                        elem.style.left = pos + 'px';
+                        expStart = (top + (restartValue * Math.sin(pos / 9)));
+                    } else {
+                        elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 9))) + 'px';
+                        pos++;
+                        elem.style.left = pos + 'px';
+                        expStart = (top + (parseInt(slider.value) * Math.sin(pos / 9)));
+                    }
+
+                } else if (element == 'proton5' && (tunneling3.checked || tunneling3True)) {
+                    // right wall traveling right
+                    if (pos < 480 + parseInt(barrier2.value) && pos > 475) {
+                        tunneling3True = true;
+                        if (expStart > top) {
+                            restartValue = (top + (expStart - top) * Math.exp(-(pos - 475) / (-900 / Math.log(0.0001 * (expStart - top)))));
+                            elem.style.top = restartValue + 'px';
+                            restartValue = restartValue % top;
+                        } else {
+                            restartValue = (top - expStart * Math.exp(-(pos - 475) / (-900 / Math.log(0.0001 * expStart))));
+                            elem.style.top = restartValue + 'px';
+                            restartValue = restartValue % top;
+                        }
+                        // travel exponentially
+                        pos++;
+                        elem.style.left = pos + 'px';
+                    } else if (pos == 875) {
+                        flag = 'stop';
+                        animationManager(flag, element, height, energy);
+                    } else if (pos >= 480 + parseInt(barrier2.value)) {
+                        elem.style.top = (top + (restartValue * Math.sin(pos / 9))) + 'px';
+                        pos++;
+                        elem.style.left = pos + 'px';
+                        expStart = (top + (restartValue * Math.sin(pos / 9)));
                     } else {
                         elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 9))) + 'px';
                         pos++;
@@ -46,45 +111,17 @@ function animationManager(flag, element, height, energy) {
                 } else if (pos == 475) {
                     goingRight = false;
                 } else {
-                    elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 10.5))) + 'px';
+                    elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 9))) + 'px';
                     pos++;
                     elem.style.left = pos + 'px';
                 }
             }
             else {
-                if (element == 'proton3') {
-                    // left wall traveling left
-                    if (pos < 100 && pos > 0) {
-                        if (expStart > top) {
-                            elem.style.top = (top + (expStart - top) * Math.exp((pos - 100) / (-125 / Math.log(0.0001 * (expStart - top))))) + 'px';
-                        } else {
-                            elem.style.top = (top - expStart * Math.exp((pos - 100) / (-125 / Math.log(0.0001 * expStart)))) + 'px';
-                        }
-                        pos--;
-                        elem.style.left = pos + 'px';
-                        // right wall traveling left
-                    } else if (pos < 575 && pos > 475) {
-                        // travel exponentially
-                        if (expStart > top) {
-                            elem.style.top = (top + (expStart - top) * Math.exp(-(pos - 475) / (-125 / Math.log(0.0001 * (expStart - top))))) + 'px';
-                        } else {
-                            elem.style.top = (top - expStart * Math.exp(-(pos - 475) / (-125 / Math.log(0.0001 * expStart)))) + 'px';
-                        }
-                        pos--;
-                        elem.style.left = pos + 'px';
-                    } else if (pos == 0) {
-                        goingRight = true;
-                    } else {
-                        elem.style.top = (top - (parseInt(slider.value) * Math.sin(pos / 9))) + 'px';
-                        pos--;
-                        elem.style.left = pos + 'px';
-                        expStart = (top - (parseInt(slider.value) * Math.sin(pos / 9)));
-                    }
-                } else if (pos == 100) {
+                if (pos == 100) {
                     goingRight = true;
                 }
                 else {
-                    elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 10.5))) + 'px';
+                    elem.style.top = (top + (parseInt(slider.value) * Math.sin(pos / 9))) + 'px';
                     pos--;
                     elem.style.left = pos + 'px';
                 }
@@ -95,6 +132,9 @@ function animationManager(flag, element, height, energy) {
         running = false;
     } else if (flag == "reset") {
         window.clearInterval(id);
+        tunneling3True = false;
+        tunneling2True = false;
+        tunneling1True = false;
         elem.style.left = 100 + 'px';
         elem.style.top = 185 + 'px';
         goingRight = true;
@@ -103,11 +143,47 @@ function animationManager(flag, element, height, energy) {
     
 }
 
-function draw() {
-    var c = document.getElementById("leftside2");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
+function changeBarrier(slider, rightwall, rightside) {
+    var wall = document.getElementById(rightwall);
+    var wallwidth = wall.style.width;
+    var side = document.getElementById(rightside);
+    var sidewidth = 296;
+    var barrier = document.getElementById(slider);
+    wall.style.width = barrier.value + 'px';
+    side.style.width = (sidewidth - (barrier.value - 100)) + 'px';    
 }
 
+function move() {
+    var elem = document.getElementById("electron1");
+    id = setInterval(frame, 10);
+    var pos = elem.offsetLeft;
+    var background = document.getElementById("background");
+    var top = 80;
+    var updateTop = 0;
+    function frame() {
+        // move in sine wave to right
+        if (pos < background.offsetWidth && pos >= 150 && electronRight == true) {
+            elem.style.top = (top + (20 * Math.sin(pos / 9))) + 'px';
+            pos++;
+            elem.style.left = pos + 'px';
+            // move in sine wave to left
+        } else if (pos < background.offsetWidth && pos >= 150 && electronRight == false) {
+            elem.style.top = (top + (20 * Math.sin(pos / 9))) + 'px';
+            pos--;
+            elem.style.left = pos + 'px';
+        } else if (pos == background.offsetWidth) {
+            electronRight = false;
+            pos--;
+        } else {
+            electronRight = true;
+            pos++;
+        }          
+    }
+}
+
+function switchParticle(url) {
+    var particle = document.getElementById("proton5");
+    particle.style.background = "url('" + url + "')"
+    particle.style.backgroundPosition = "center";
+    particle.style.backgroundSize = 30 + 'px';
+}
